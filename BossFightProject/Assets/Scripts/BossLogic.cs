7e8115129ecb9c2178f84d7751a0e5d576ff498c
James-LiSooey <jlisooey@gmail.com>
2020-05-09 13:34:04 -0400

    Boss turning and moving

--- BossFightProject/Assets/Scripts/BossLogic.cs
+++ BossFightProject/Assets/Scripts/BossLogic.cs
@@ -1,18 +1,150 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using UnityEngine;
+
+enum BossState
+{
+    Idle,
+    Moving,
+    Attacking,
+    Turning
+}
+enum BossAttackState
+{
+    NoAttack,
+    JumpAttack,
+    SwipeAttack,
+    SlamAttack,
+    KickAttack
+}
 
 public class BossLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    BossState m_bossState = BossState.Idle;
+    BossAttackState bossAttackState = BossAttackState.NoAttack;
+    CharacterController m_characterController;
+    Animator m_animator;
+    GameObject m_player;
+
+    Vector3 m_movementTarget;
+    Vector3 m_horizontalMovement;
+    Vector3 m_verticalMovement;
+    Vector3 m_heightMovement;
+    public float m_movementSpeed = 2.0f;
+    public float m_distanceToTarget;
+    float m_gravity = 0.01f;
+
+    public float m_turnAngle = 20f;
+    public float m_stoppingDistance = 3.0f;
+
     void Start()
     {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_characterController = GetComponent<CharacterController>();
+        m_animator = GetComponent<Animator>();
+        m_movementTarget = transform.position;
+    }
+
+    void Update()
+    {
+        m_movementTarget = m_player.transform.position;
+
+        var heading = m_movementTarget - transform.position;
+        var distanceToTarget = heading.magnitude;
+        var direction = heading / distanceToTarget;
+
+        var correctedDirection = new Vector3(direction.x, 0f, direction.z);
+
+        m_distanceToTarget = Vector3.Distance(m_movementTarget, transform.position);
+        var angleDifference = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(correctedDirection));
+
+        var newLookAt = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(correctedDirection), 2.0F * Time.deltaTime);
+
+
+        //Debug.Log("Angle difference: " + angleDifference);
+        Debug.Log("Current Rotation: " + transform.rotation.eulerAngles.y);
+        
+
+        if (!m_characterController.isGrounded)
+        {
+            m_heightMovement.y -= m_gravity;
+        }
+        else
+        {
+            m_heightMovement.y = 0;
+        }
+
+        Vector3 movementVector = new Vector3(direction.x * m_movementSpeed * Time.deltaTime, m_heightMovement.y, direction.z * m_movementSpeed * Time.deltaTime);
+
+        if(angleDifference > m_turnAngle)
+        {
+            
+            m_bossState = BossState.Turning;
+        }
+        else if (m_distanceToTarget > 3)
+        {
+            m_bossState = BossState.Moving;
+        }
+        else
+        {
+            m_bossState = BossState.Idle;
+        }
+
+        switch (m_bossState)
+        {
+            case BossState.Turning:
+                var currentAngle = transform.rotation.eulerAngles.y;
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(correctedDirection), 1.2F * Time.deltaTime);
+                var newAngle = transform.rotation.eulerAngles.y;
+
+                if(currentAngle<90 && newAngle > 270)
+                {
+                    newAngle *= -1;
+                }else if(currentAngle > 270 && newAngle<90)
+                {
+                    currentAngle *= -1;
+                }
+
+                if(currentAngle - newAngle < 0)
+                {
+                    m_animator.SetFloat("TurnInput", .5f);
+                }
+                else
+                {
+                    m_animator.SetFloat("TurnInput", -.5f);
+                }
+                m_animator.SetFloat("MovementInput", 0f);
+
+                break;
+            case BossState.Moving:
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(correctedDirection), 2F * Time.deltaTime);
+                m_characterController.Move(movementVector);
+                m_animator.SetFloat("TurnInput", 0f); 
+                m_animator.SetFloat("MovementInput", 1f);
+                break;
+            case BossState.Idle:
+                m_animator.SetFloat("TurnInput", 0f);
+                m_animator.SetFloat("MovementInput", 0f);
+                break;
+            default:
+                m_animator.SetFloat("TurnInput", 0f);
+                m_animator.SetFloat("MovementInput", 0f);
+                break;
+        }
+
+        
+
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoveTowardsPoint(Vector3 pos)
     {
-        
+        if (m_characterController)
+        {
+            m_characterController.Move(pos* m_movementSpeed*Time.deltaTime);
+        }
     }
 }
---
+++ BossFightProject/Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    CharacterController m_characterController;
+
+    float m_movementSpeed = 5.0f;
+    float m_horizontalInput;
+    float m_verticalInput;
+
+    Vector3 m_horizontalMovement;
+    Vector3 m_verticalMovement;
+    Vector3 m_heightMovement;
+
+    float m_jumpHeight = .3f;
+    float m_gravity = 0.01f;
+    bool m_jump = false;
+
+    Animator m_animator;
+
+    [SerializeField]
+    List<AudioClip> m_FootstepEarthSounds = new List<AudioClip>();
+    [SerializeField]
+    List<AudioClip> m_FootstepGrassSounds = new List<AudioClip>();
+
+    AudioSource m_AudioSource;
+
+    [SerializeField]
+    Transform m_LeftFoot;
+
+    [SerializeField]
+    Transform m_RightFoot;
+
+
+    GameObject m_Camera;
+    CameraLogic m_CameraLogic;
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_characterController = GetComponent<CharacterController>();
+        m_animator = GetComponent<Animator>();
+        m_AudioSource = GetComponent<AudioSource>();
+        m_Camera = Camera.main.gameObject;
+        if (m_Camera)
+        {
+            m_CameraLogic = m_Camera.GetComponent<CameraLogic>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_verticalInput = Input.GetAxis("Vertical");
+
+        if (!m_jump && Input.GetButtonDown("Jump"))
+        {
+            m_jump = true;
+        }
+
+        if (m_animator) {
+            m_animator.SetFloat("HorizontalInput", m_horizontalInput);
+            m_animator.SetFloat("VerticalInput", m_verticalInput);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if(m_CameraLogic && (Mathf.Abs(m_horizontalInput)>0.1f || Mathf.Abs(m_verticalInput) > 0.1f))
+        {
+            transform.forward = m_CameraLogic.GetForwardVector();
+        }
+
+        m_horizontalMovement = transform.right * m_horizontalInput * m_movementSpeed * Time.deltaTime;
+        m_verticalMovement = transform.forward * m_verticalInput * m_movementSpeed * Time.deltaTime;
+
+        if (!m_characterController.isGrounded)
+        {
+            m_heightMovement.y -= m_gravity;
+        }
+        else
+        {
+            m_heightMovement.y = 0;
+        }
+        if (m_jump)
+        {
+            m_heightMovement.y = m_jumpHeight;
+            m_jump = false;
+        }
+
+        if (m_characterController)
+        {
+            m_characterController.Move(m_horizontalMovement + m_verticalMovement + m_heightMovement);
+        }
+
+    }
+
+    public void PlayFootstepSound(int footIndex)
+    {
+        // 0 = left, 1 = right
+
+        if (footIndex == 0)
+        {
+            RayCastTerrain(m_LeftFoot.position);
+        }else if(footIndex == 1)
+        {
+            RayCastTerrain(m_RightFoot.position);
+        }
+    }
+
+    void RayCastTerrain(Vector3 position)
+    {
+        LayerMask layerMask = LayerMask.GetMask("Terrain");
+        Ray ray = new Ray(position, Vector3.down);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit, layerMask))
+        {
+            string hitTag = hit.collider.gameObject.tag;
+            if(hitTag == "Earth")
+            {
+                PlayRandomSound(m_FootstepEarthSounds);
+            }
+            else if (hitTag == "Grass")
+            {
+                PlayRandomSound(m_FootstepGrassSounds);
+            }
+            else
+            {
+                PlayRandomSound(m_FootstepEarthSounds);
+            }
+            
+        }
+    }
+
+    void PlayRandomSound(List<AudioClip> audioClips)
+    {
+        if (audioClips.Count > 0 && m_AudioSource)
+        {
+            int randomNum = Random.Range(0, audioClips.Count - 1);
+
+            m_AudioSource.PlayOneShot(audioClips[randomNum]);
+        }
+    }*/
+
+}
