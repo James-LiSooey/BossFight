fc400fe4616ee5d164348eee1461118cd181ea01
James-LiSooey <jlisooey@gmail.com>
2020-05-24 14:47:33 -0400

    imported Audio Start Menu created

---
+++ BossFightProject/Assets/Scripts/StartMenuLogic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenuLogic : MonoBehaviour
+{
+
+    [SerializeField]
+    Button m_startButton;
+    [SerializeField]
+    Button m_exitButton;
+
+    [SerializeField]
+    GameObject m_TransitionCamera;
+    [SerializeField]
+    GameObject m_startMenu;
+
+    [SerializeField]
+    GameObject m_bossHealth;
+    [SerializeField]
+    GameObject m_playerHealth;
+    // Start is called before the first frame update
+     void Start()
+    {
+        if (m_startButton)
+        {
+            m_startButton.Select();
+        }
+    }
+
+    public void OnStartClicked()
+    {
+        m_TransitionCamera.SetActive(true);
+        m_bossHealth.SetActive(true);
+        m_playerHealth.SetActive(true);
+        m_startMenu.SetActive(false);
+    }
+
+    public void OnExitClicked()
+    {
+        Application.Quit();
+    }
+
+    public void DisableAllChildren(){
+
+    }
+}
---
+++ BossFightProject/Assets/Scripts/TransitionCameraLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCameraLogic : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject m_freelookCamera;
+    [SerializeField]
+    GameObject m_player;
+    [SerializeField]
+    GameObject m_boss;
+
+    PlayerLogic m_playerLogic;
+
+    public float m_transitionTimer = 0;
+    float m_transitionTimelimit = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_boss.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_transitionTimer<=m_transitionTimelimit){
+            m_transitionTimer += Time.fixedDeltaTime;
+        }
+        if(m_transitionTimer>m_transitionTimelimit-.5f){
+            m_freelookCamera.SetActive(true);
+        }
+        if(m_transitionTimer>m_transitionTimelimit){
+            m_playerLogic = m_player.GetComponent<PlayerLogic>();
+            m_playerLogic.enabled = true;
+        }
+    }
+}
